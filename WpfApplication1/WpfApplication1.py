1c80671dca46feab9fcd4500f4d13e04551389ae
DronSX <50011397+DronSX@users.noreply.github.com>
2019-06-29 21:14:39 +0300

    Добавьте файлы проекта.

---
+++ WpfApplication1/WpfApplication1.py
@@ -0,0 +1,17 @@
+﻿import wpf
+
+from System.Windows import Application, Window
+
+class MyWindow(Window):
+    def __init__(self):
+        wpf.LoadComponent(self, 'WpfApplication1.xaml')
+        
+    def Button_Click(self, sender, e):
+        pass
+
+    
+    def Button_Click1(self, sender, e):
+        pass
+
+if __name__ == '__main__':
+    Application().Run(MyWindow())
